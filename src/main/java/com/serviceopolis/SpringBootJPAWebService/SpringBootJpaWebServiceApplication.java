c0c284dd14551290f5536d503749f0261047772d
LeanBlauth <le4ndroblauth@gmail.com>
2021-05-17 22:25:03 -0300

    Project created

---
+++ src/main/java/com/serviceopolis/SpringBootJPAWebService/SpringBootJpaWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.serviceopolis.SpringBootJPAWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaWebServiceApplication.class, args);
+	}
+
+}
