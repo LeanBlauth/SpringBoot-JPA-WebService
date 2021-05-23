7634db602ea3a3afc1615ad9381ea6ba6f1e1447
LeanBlauth <le4ndroblauth@gmail.com>
2021-05-22 22:25:11 -0300

    OrderItem: method getSubTotal() ; Order: method getTotal(); @JsonIgnore on Order-Payment association

--- src/main/java/com/serviceopolis/SpringBootJPAWebService/entities/Order.java
+++ src/main/java/com/serviceopolis/SpringBootJPAWebService/entities/Order.java
@@ -94,6 +94,14 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		Double sum = 0.0;
+		for(OrderItem item : items) {
+			sum += item.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
